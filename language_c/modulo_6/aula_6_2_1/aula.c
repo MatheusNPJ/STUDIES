5342f57b10bd12a1d33a545bfd4598d8a729cabc
NPJ Matheus <129205343+MatheusNPJ@users.noreply.github.com>
2023-11-02 22:20:02 +0000

    progressing in studies

--- language_c/modulo_6/aula_6_2_1/aula.c
+++ language_c/modulo_6/aula_6_2_1/aula.c
@@ -5,6 +5,7 @@ int main (){
     char s [10];
 
     printf("Digite algo (leitura pelo gets):\n");
+    //GETS NAO CONTROLA TAMANHO DE LIMITE DE VETOR.
     gets(s);
     fflush(stdin);
 
@@ -13,6 +14,7 @@ int main (){
     puts("");
 
     printf("Digite algo (leitura pelo fgets):\n");
+    //FGETS CONTROLA, SO IRA IMPRIMIR OS 10 CARACTERES.
     fgets(s, 10, stdin);
     fflush(stdin);
 
---
+++ language_c/modulo_7/aula_7_1/aula.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+//COMPARANDO STRINGS USANDO STRCMP.
+
+//Declarando uma constante.
+#define N 50
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+}
