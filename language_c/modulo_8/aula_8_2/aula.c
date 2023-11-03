363031c589f1e372607edbb3d499608a4318927f
NPJ Matheus <129205343+MatheusNPJ@users.noreply.github.com>
2023-11-03 16:38:59 +0000

    Progressing in studies

--- language_c/modulo_8/aula_8_2/aula.c
+++ language_c/modulo_8/aula_8_2/aula.c
@@ -7,6 +7,8 @@
 
 //CRIANDO UM VETOR DE STRUCTS (MUITO BOM).
 
+//COMPILE E ENTENDA. 
+
 struct tipo_pessoa{
     int idade;
     float peso;
@@ -24,7 +26,26 @@ int main(){
     for(i = 0; i < TAM; i++){
         printf("Insira os dados (%d):\n", i+1);
         puts("Nome: ");
-        scanf("%50[^\n]s", &lista[i].nome);
+        scanf("%50s", &lista[i].nome);
+        fflush(stdin);
+
+        puts("Idade: ");
+        scanf("%ds", &lista[i].idade);
+        fflush(stdin);
+
+        puts("Peso: ");
+        scanf("%f", &lista[i].peso);
+        fflush(stdin);
     }
+    system("cls");//ESTE COMANDO ERA PARA APAGAR O QUE POR MIM FOI INSERIDO ATÉ O MOMENTO NESTE CÓDIGO, MAS NÃO FUNCIONOU.
+
+    puts("Seus dados: \n");
+    for(i = 0; i < TAM; i++){
+        printf("-----------Pessoa %d -----------\n", i+1);
+        printf("\tNome: %s\n", lista[i].nome);
+        printf("\tIdade: %d\n", lista[i].idade);
+        printf("\tPeso: %.2f\n", lista[i].peso);
+    }
+    printf("-------------------------------------\n");
 }
 
